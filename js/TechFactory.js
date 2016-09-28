3669f8476d243c7520423579da157930d02819a9
Alex Horton <alexhortonmusic@gmail.com>
2016-09-28 00:35:27 -0500

    added stack images

---
+++ js/TechFactory.js
@@ -0,0 +1,69 @@
+'use strict';
+
+app.factory('TechFactory', function ($http) {
+
+  const getTechImgArr = () => {
+    // const techImgArr = [
+    //   {
+    //     "imgUrl": "/img/tech/angular.png"
+    //   },
+    //   {
+    //     "imgUrl": "/img/tech/bower.png"
+    //   },
+    //   {
+    //     "imgUrl": "/img/tech/browserify.png"
+    //   },
+    //   {
+    //     "imgUrl": "/img/tech/css3.png"
+    //   },
+    //   {
+    //     "imgUrl": "/img/tech/express.png"
+    //   },
+    //   {
+    //     "imgUrl": "/img/tech/firebase.png"
+    //   },
+    //   {
+    //     "imgUrl": "/img/tech/git.png"
+    //   },
+    //   {
+    //     "imgUrl": "/img/tech/github.png"
+    //   },
+    //   {
+    //     "imgUrl": "/img/tech/gulp.png"
+    //   },
+    //   {
+    //     "imgUrl": "/img/tech/html5.png"
+    //   },
+    //   {
+    //     "imgUrl": "/img/tech/jasmine.png"
+    //   },
+    //   {
+    //     "imgUrl": "/img/tech/jq2.png"
+    //   },
+    //   {
+    //     "imgUrl": "/img/tech/jslogo.png"
+    //   },
+    //   {
+    //     "imgUrl": "/img/tech/mocha.png"
+    //   },
+    //   {
+    //     "imgUrl": "/img/tech/mongo.png"
+    //   },
+    //   {
+    //     "imgUrl": "/img/tech/nodejs.png"
+    //   },
+    //   {
+    //     "imgUrl": "/img/tech/npm.png"
+    //   },
+    //   {
+    //     "imgUrl": "/img/tech/pugface.png"
+    //   },
+    //   {
+    //     "imgUrl": "/img/tech/sass.png"
+    //   }
+    // ]
+  }
+
+  // return { getTechImgArr }
+
+})
