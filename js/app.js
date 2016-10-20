96b400e80f2a6148fa7add07a4beb4202a0c7692
Alex Horton <alexhortonmusic@gmail.com>
2016-10-20 13:58:17 -0500

    fixed modal/work functionality

--- js/app.js
+++ js/app.js
@@ -53,15 +53,19 @@ const Tradesies = {
   className: 'tradesies',
   mainImg: '../img/tradesies-home.png',
   info: 'A gear-trading application for the restless musician.',
-  img2: '../img/tradesies-community.png'
+  img2: '../img/tradesies-community.png',
+  url: '#',
+  github: 'https://github.com/alexhortonmusic/tradesies/tree/master/wholeDangThing'
 }
 
 const SpaceRace = {
   name: 'Space Race',
   className: 'spaceRace',
   mainImg: '../img/space-game.png',
-  info: 'Space Race is a live-action multiplayer game, supporting multiple users and rooms. Build using technologies such as Node.js, Express, Socket.io, and Pug, Space Race is a game where 2-4 players race their ships to the moon.',
-  img2: '../img/space-home.png'
+  info: 'Space Race is a live-action multiplayer game, supporting multiple users and rooms. Built using technologies such as Node.js, Express, Socket.io, and Pug, Space Race is a game where 2-4 players race their ships to the moon.',
+  img2: '../img/space-home.png',
+  url: 'https://space-race-.herokuapp.com/',
+  github: 'https://github.com/alexhortonmusic/space-race'
 }
 
 const Freddit = {
@@ -69,30 +73,47 @@ const Freddit = {
   className: 'freddit',
   mainImg: '../img/freddit-home.png',
   info: 'Freddit is a social news aggregator and rating site. In other words, a clone of the basic features of Reddit.',
-  img2: '../img/freddit-comment.png'
+  img2: '../img/freddit-comment.png',
+  url: 'https://ancient-hamlet-94779.herokuapp.com/',
+  github: 'https://github.com/alexhortonmusic/fake-reddit'
 }
 
 projectsArr.push(Tradesies, SpaceRace, Freddit)
 
-const projectHTMLarr = []
+const projectHTMLArr = []
+const projectModalArr = []
 
 projectsArr.forEach((project, index) => {
   const clickableHTML = `
-    <li class='test'>
+    <li class='modal ${project.className}'>
       <h2>${project.name}</h2>
-      <a href="#">
+      <div>
         <img src="${project.mainImg}" alt="${project.name} screenshot">
-      </a>
+      </div>
     </li>
   `
-  projectHTMLarr.push(clickableHTML)
+  const modalHTML = `
+    <div id='${project.className}' class='modalPop'>
+      <h2>${project.name}</h2>
+      <p>${project.info}</p>
+      <div class='links'>
+        <a href='${project.url}'>Try it out!</a>
+        <a href='${project.github}'>Check out the code</a>
+      </div>
+      <img src="${project.mainImg}" alt="${project.name} Home Page"/>
+      <img src="${project.img2}" alt="${project.name} Second Page"/>
+    </div>
+  `
+  projectHTMLArr.push(clickableHTML)
+  projectModalArr.push({ name: `${project.className}`, modal: modalHTML})
 })
 
-projectHTMLarr.forEach((projectHTML) => {
-  // $('.modalBox').append(projectHTML)
+// projects to DOM
+projectHTMLArr.forEach((projectHTML) => {
   $('.galleryUl').append(projectHTML)
 })
 
-
-// use this to build modalzzzz
-$('.test').featherlight('<h1>Hello</h1>')
+// build modals
+projectModalArr.forEach(({ name, modal }) => {
+  $(`.${name}`).featherlight(modal)
+})
